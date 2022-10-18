a288db52720168092588a6636c2f79cb9361240f
nordgaren <nordgarentv@gmail.com>
2022-10-17 20:37:22 -0700

    Fixed a few things with Chr/Enemy and added ERR items as optional resources.

---
+++ src/Erd-Tools/Assembly/GetChrInsByHandle.asm
@@ -0,0 +1,6 @@
+﻿mov rcx,0x{0:X2}            ;WorldChrManPtr	
+mov rdx,0x{1:X2}            ;ChrHandlePtr	
+call 0x{2:X2}               ;GetChrInsByHandle
+mov rcx, 0x{3:X2}
+mov [rcx], rax    ;Move return to buffer		
+ret
